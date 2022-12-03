07e5551b46ddc573113892bf8b867596050ef3b4
Srinivas Reddy Onteddu <srinivasr.techie@gmail.com>
2022-12-03 00:31:19 -0600

    Initial Commit for custom best price promotions extensions.

---
+++ hybris/bin/custom/bestprice/bestpricepromotions/bestpricecouponfacades/src/com/bestprice/coupon/facades/constants/BestpricecouponfacadesConstants.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.bestprice.coupon.facades.constants;
+
+/**
+ * Global class for all Bestpricecouponfacades constants. You can add global constants for your extension into this class.
+ */
+public final class BestpricecouponfacadesConstants extends GeneratedBestpricecouponfacadesConstants
+{
+	public static final String EXTENSIONNAME = "bestpricecouponfacades";
+
+	private BestpricecouponfacadesConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
